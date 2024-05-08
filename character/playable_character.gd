43bd02b4d9ceec03cceef708baa450f8f36a924d
Hectorv72 <hectorvaldezfsa13@gmail.com>
2024-05-08 00:47:29 -0300

    agrega movimiento libre de 4 direcciones

--- character/playable_character.gd
+++ character/playable_character.gd
@@ -5,6 +5,8 @@ const speed = 400
 const acceleration = 10
 
 var key_is_pressed = false
+var key_x_is_pressed = false
+var key_y_is_pressed = false
 
 const JUMP_VELOCITY = -400.0
 @onready var character_sprite = $CharacterSprite
@@ -19,17 +21,22 @@ func _physics_process(delta):
 	# Add the gravity.
 	#if not is_on_floor():
 		#velocity.y += gravity * delta
-	print(velocity.x)
-	if (velocity.x > 2):
-		if(key_is_pressed):
-			character_sprite.play("move_forward")
-		else:
+	#print(velocity.x)
+	if (velocity.x > 1):
+		if(!key_x_is_pressed and !key_y_is_pressed):
 			character_sprite.play("forward_decelerate")
-		#character_sprite.animation = "move_forward"
-	elif (velocity.x < -2):
-		character_sprite.play("move_backward")
+	elif (velocity.x < -1):
+		if(!key_x_is_pressed and !key_y_is_pressed):
+			character_sprite.play("decelerate")
+	elif (velocity.y > 1):
+		if(!key_x_is_pressed and !key_y_is_pressed):
+			character_sprite.play("decelerate")
+	elif (velocity.y < -1):
+		if(!key_x_is_pressed and !key_y_is_pressed):
+			character_sprite.play("decelerate")
 	else:
-		character_sprite.play("idle")
+		if(!key_x_is_pressed and !key_y_is_pressed):
+			character_sprite.play("idle")
 	## Handle jump.
 	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		#velocity.y = JUMP_VELOCITY
@@ -37,14 +44,33 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	#var direction = Input.get_axis("ui_left", "ui_right")
-	key_is_pressed = false
+	
 	if (Input.is_action_pressed("ui_right")):
-		key_is_pressed = true
+		key_x_is_pressed = true
+		character_sprite.play("move_forward")
 		velocity.x = min(velocity.x + acceleration, speed)
 	elif (Input.is_action_pressed("ui_left")):
+		key_x_is_pressed = true
+		character_sprite.play("move_backward")
 		velocity.x = max(velocity.x - acceleration, -speed)
 	else:
+		key_x_is_pressed = false
 		velocity.x = lerp(velocity.x,0.0,0.05)
+	
+	
+	if (Input.is_action_pressed("ui_up")):
+		key_y_is_pressed = true
+		if(!key_x_is_pressed):
+			character_sprite.play("move_up")
+		velocity.y = max(velocity.y - acceleration, -speed)
+	elif (Input.is_action_pressed("ui_down")):
+		key_y_is_pressed = true
+		if(!key_x_is_pressed):
+			character_sprite.play("move_down")
+		velocity.y = min(velocity.y + acceleration, speed)
+	else:
+		key_y_is_pressed = false
+		velocity.y = lerp(velocity.y,0.0,0.05)
 	
 	#print(direction)
 	#if(starting_run):
@@ -57,7 +83,8 @@ func _physics_process(delta):
 
 func _on_character_sprite_animation_looped():
 	var animation = character_sprite.animation
-	var list_animations = ["move_forward","move_backward","forward_decelerate"]
+	print(animation)
+	var list_animations = ["move_forward","move_backward","move_up","move_down","forward_decelerate"]
 	if(animation in list_animations):
 		starting_run = true
 		character_sprite.frame = character_sprite.sprite_frames.get_frame_count(character_sprite.animation)
